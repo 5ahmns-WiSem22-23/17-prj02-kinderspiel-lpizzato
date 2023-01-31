3add00793de9f11c68267c8f2e06f4c4fb65a266
lindixxx <linda.pizzato03@htl-salzburg.ac.at>
2023-01-31 15:58:05 +0100

    Counter and Sprites

--- Assets/Affenbande/Scripts/MonkeyManager.cs
+++ Assets/Affenbande/Scripts/MonkeyManager.cs
@@ -1,63 +1,68 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MonkeyManager : MonoBehaviour
 {
-	[SerializeField] GameObject[] monkeys;
-	[SerializeField] Button btn;
-	int random;
+	// [SerializeField] GameObject[] monkeys;
+    [SerializeField] List<GameObject> monkeys = new List<GameObject>();
+
+    [SerializeField] Button btn;
     [SerializeField] Text turnText;
+
+    int random;
+    [SerializeField] int color1, color2, color3, color4;
     [SerializeField] float animationTime;
+    [SerializeField] Vector3 finalPos1, finalPos2, finalPos3, finalPos4;
+
+    string tagColor;
 
 	void Start()
 	{
         btn.onClick.AddListener(TaskOnClick);
 	}
 
-    // animate monkey in the middle
-    // move it to spielbrett from it's color
-    // sprites: spielbrett, monkey, button
     // button only activated when playing
     // two players one after each other? how?
     // reihenfolge wohin es gehen soll beim ziehen: position immer + 2 oder so
-    // don't take monkey twice -> if number already chosen, don't choose again
+    // beginning: sprechblasen aus dem wald in der mitte
+    // drag & drop monkey -> snap to position
+    // every time one monkey from one color gets chosen -> int++
+    // switch int -> position always +2 idk
+    // menu
 
     public void TaskOnClick()
 	{
-        random = Random.Range(0, monkeys.Length);
+        random = Random.Range(0, monkeys.Count);
 
-        if (monkeys[random].CompareTag("Color1"))
+        tagColor = monkeys[random].tag;
+        switch (tagColor)
         {
-            GetAMonkey(new Vector3(-4, 2, 0));
-
-			// Debug.Log("random: " + random + ", Color1");
+            case "Color1":
+                color1++;
+                GetAMonkey(finalPos1, color1);
+                break;
+            case "Color2":
+                color2++;
+                GetAMonkey(finalPos2, color2);
+                break;
+            case "Color3":
+                color3++;
+                GetAMonkey(finalPos3, color3);
+                break;
+            case "Color4":
+                color4++;
+                GetAMonkey(finalPos4, color4);
+                break;
         }
-        else if (monkeys[random].CompareTag("Color2"))
-        {
-            GetAMonkey(new Vector3(-4, -2, 0));
+    }
 
-            // Debug.Log("random: " + random + ", Color2");
-        }
-        else if (monkeys[random].CompareTag("Color3"))
-        {
-            GetAMonkey(new Vector3(4, 2, 0));
-
-            // Debug.Log("random: " + random + ", Color3");
-        }
-        else if (monkeys[random].CompareTag("Color4"))
-        {
-            GetAMonkey(new Vector3(-4, 2, 0));
-
-            // Debug.Log("random: " + random + ", Color4");
-        }
-	}
-
-    void GetAMonkey(Vector3 position)
+    void GetAMonkey(Vector3 position, int count)
     {
-        GameObject color = monkeys[random].gameObject;
-        // color.SetActive(true);
-        LeanTween.scale(color, new Vector3(0.2f, 0.2f, 0), animationTime);
+        GameObject color = monkeys[random];
+        monkeys.RemoveAt(random);
+        LeanTween.scale(color, new Vector3(0.25f, 0.25f, 0), animationTime);
         LeanTween.move(color, position, animationTime).setDelay(4f);
-        LeanTween.scale(color, new Vector3(0.13f, 0.13f, 0), 2f).setDelay(4f);
+        LeanTween.scale(color, new Vector3(0.13f, 0.13f, 0), animationTime).setDelay(4f);
     }
 }
